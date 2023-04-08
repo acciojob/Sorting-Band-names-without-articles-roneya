9378f6797a61f3ae091e356dbfa9f4390450c8b4
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-08 19:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here
+
+let bandNames = ["The Beatles", "Led Zeppelin", "Pink Floyd", "Aerosmith", "The Who"];
+
+function ra(arr) {
+  return arr.replace(/^(a |an |the )/i, "").trim();
+}
+
+const sortedBandNames = bandNames.sort((a, b) => {
+  const nameA = removeArticles(a);
+  const nameB = removeArticles(b);
+  if (nameA < nameB) {
+    return -1;
+  } else if (nameA > nameB) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
 
